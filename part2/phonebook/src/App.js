b9e1eb4b60d51461ba0a62b1d95df0bc0b3a89da
Minel Drda <94012283+nusretWazowski@users.noreply.github.com>
2022-07-11 20:07:03 +0200

    Add files via upload

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+  const addNumberHandler = (event) => {
+    event.preventDefault();
+    const numberObject = { name: newName };
+    setPersons(persons.concat(numberObject))
+    setNewName("");
+  };
+
+  const changeNameHandler = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNumberHandler}>
+        <div>
+          name: <input value={newName} onChange={changeNameHandler} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person) => (
+          <p key={person.name}>{person.name}</p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
