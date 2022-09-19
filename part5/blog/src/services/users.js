0a0d0765a5ca11dde936ae88daabbd117df7025c
Minel Drda <94012283+nusretWazowski@users.noreply.github.com>
2022-09-19 02:07:33 +0100

    Delete part5/blog/src directory

--- part5/blog/src/services/users.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const baseUrl = '/api/users';
-
-const login = async (credentials) => {
-    const user = await axios.post(`${baseUrl}/login`, credentials);
-    return user.data;
-};
-
-export default { login };
