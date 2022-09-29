3e7770998f21f34dad666f69fc5099a5546ec8f0
Minel Drda <94012283+nusretWazowski@users.noreply.github.com>
2022-09-29 20:15:59 +0100

    Add files via upload

--- part8/library/backend/index.js
+++ part8/library/backend/index.js
@@ -6,94 +6,18 @@ const {
 } = require('apollo-server');
 const { mongoose } = require('mongoose');
 
+require('dotenv').config();
+
 const Book = require('./models/book');
 const Author = require('./models/author');
 const User = require('./models/user');
 const jwt = require('jsonwebtoken');
 
-const MONGODB_URL =
-    'mongodb+srv://cookerdewitt:sarajevo123@cluster0.z4xvgfr.mongodb.net/library?retryWrites=true&w=majority';
+const MONGODB_URL = process.env.MONGODB_URL;
+
 const SECRET = 'SECRET';
 
 mongoose.connect(MONGODB_URL).then(() => console.log('Connected to database.'));
-
-// let authors = [
-//     {
-//         name: 'Robert Martin',
-//         id: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
-//         born: 1952,
-//     },
-//     {
-//         name: 'Martin Fowler',
-//         id: 'afa5b6f0-344d-11e9-a414-719c6709cf3e',
-//         born: 1963,
-//     },
-//     {
-//         name: 'Fyodor Dostoevsky',
-//         id: 'afa5b6f1-344d-11e9-a414-719c6709cf3e',
-//         born: 1821,
-//     },
-//     {
-//         name: 'Joshua Kerievsky', // birthyear not known
-//         id: 'afa5b6f2-344d-11e9-a414-719c6709cf3e',
-//     },
-//     {
-//         name: 'Sandi Metz', // birthyear not known
-//         id: 'afa5b6f3-344d-11e9-a414-719c6709cf3e',
-//     },
-// ];
-
-// let books = [
-//     {
-//         title: 'Clean Code',
-//         published: 2008,
-//         author: 'Robert Martin',
-//         id: 'afa5b6f4-344d-11e9-a414-719c6709cf3e',
-//         genres: ['refactoring'],
-//     },
-//     {
-//         title: 'Agile software development',
-//         published: 2002,
-//         author: 'Robert Martin',
-//         id: 'afa5b6f5-344d-11e9-a414-719c6709cf3e',
-//         genres: ['agile', 'patterns', 'design'],
-//     },
-//     {
-//         title: 'Refactoring, edition 2',
-//         published: 2018,
-//         author: 'Martin Fowler',
-//         id: 'afa5de00-344d-11e9-a414-719c6709cf3e',
-//         genres: ['refactoring'],
-//     },
-//     {
-//         title: 'Refactoring to patterns',
-//         published: 2008,
-//         author: 'Joshua Kerievsky',
-//         id: 'afa5de01-344d-11e9-a414-719c6709cf3e',
-//         genres: ['refactoring', 'patterns'],
-//     },
-//     {
-//         title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-//         published: 2012,
-//         author: 'Sandi Metz',
-//         id: 'afa5de02-344d-11e9-a414-719c6709cf3e',
-//         genres: ['refactoring', 'design'],
-//     },
-//     {
-//         title: 'Crime and punishment',
-//         published: 1866,
-//         author: 'Fyodor Dostoevsky',
-//         id: 'afa5de03-344d-11e9-a414-719c6709cf3e',
-//         genres: ['classic', 'crime'],
-//     },
-//     {
-//         title: 'The Demon ',
-//         published: 1872,
-//         author: 'Fyodor Dostoevsky',
-//         id: 'afa5de04-344d-11e9-a414-719c6709cf3e',
-//         genres: ['classic', 'revolution'],
-//     },
-// ];
 
 const typeDefs = gql`
     type User {
@@ -156,19 +80,21 @@ const resolvers = {
         },
         allBooks: async (root, args) => {
             if (!args.author && !args.genre) {
-                return Book.find({});
+                return Book.find({}).populate('author');
             }
             if (args.author && args.genre) {
                 return Book.find({
                     author: args.author,
                     genres: { $in: [args.genre] },
-                });
+                }).populate('author');
             }
             if (args.author) {
-                return Book.find({ author: args.author });
+                return Book.find({ author: args.author }).populate('author');
             }
             if (args.genre) {
-                return Book.find({ genres: { $in: [args.genre] } });
+                return Book.find({ genres: { $in: [args.genre] } }).populate(
+                    'author'
+                );
             }
         },
         allAuthors: async () => {
@@ -197,7 +123,7 @@ const resolvers = {
                     return await book.save();
                 }
                 book.author = author._id;
-                await book.save().popula;
+                await book.save();
             } catch (error) {
                 throw new UserInputError(error.message, {
                     invalidArgs: args,
@@ -209,18 +135,17 @@ const resolvers = {
             if (!currentUser) {
                 throw new AuthenticationError('Unauthorized');
             }
-            const author = Author.findOneAndUpdate(
-                { name: args.name },
-                { born: args.setBornTo }
-            );
             try {
-                await author.save();
+                const author = await Author.findOneAndUpdate(
+                    { name: args.name },
+                    { born: args.setBornTo }
+                );
+                return author;
             } catch (error) {
                 throw new UserInputError(error.message, {
                     invalidArgs: args,
                 });
             }
-            return author;
         },
         createUser: async (root, args) => {
             const user = new User({ ...args });
