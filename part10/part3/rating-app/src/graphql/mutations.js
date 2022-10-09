fa0a441f172af895281e8bc43180bab21b32a766
Minel Drda <94012283+nusretWazowski@users.noreply.github.com>
2022-10-09 23:33:44 +0100

    Add files via upload

---
+++ part10/part3/rating-app/src/graphql/mutations.js
@@ -0,0 +1,10 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+    mutation authenticate($credentials: AuthenticateInput) {
+        authenticate(credentials: $credentials) {
+            accessToken
+        }
+    }
+`;
+    
